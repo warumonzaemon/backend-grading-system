6d1901f74a80e49745e19d060e374b8076fe2f7e
warumonzaemon <83221780+warumonzaemon@users.noreply.github.com>
2021-10-28 07:54:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 8000;
+
+const UserRouter = require('./routes/user');
+const StudentGradesRouter = require('./routes/studentGrades');
+ 
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const mongoose = require('mongoose');
+/* mongoose.connect('mongodb://localhost:27017/gradingSystem', { */
+mongoose.connect('mongodb+srv://uplift_db:uplift1234@cluster0.ul9rq.mongodb.net/gradingSystem?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.get('/', (req,res) => {
+    res.send("project 6 server is working");
+})
+
+app.use('/user', UserRouter);
+app.use('/studentGrades', StudentGradesRouter);
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`) ;})
